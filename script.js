e6ad70911cb4e7356be1f435afe0df1719bd3cdb
giambal <gimmy_1996@hotmail.it>
2019-03-20 17:25:56 +0100

    gennaio stampato

--- script.js
+++ script.js
@@ -18,14 +18,14 @@ function title(month,year) {
   var title=$("#title");
 
   var monthName=getMonthName(month);
-  var dayCount=daysCount(year,month);
+  var dayCount=daysCount(month,year);
   title.text(monthName + ": " + "1-" + dayCount);
 
 }
 
 // ---- FUNZIONE PER CONTARE QUANTI GIORNI HA IL MESE ----
 
-function daysCount(year,month) {
+function daysCount(month,year) {
 
   var mom=moment();
   mom.month(month);
@@ -35,7 +35,45 @@ function daysCount(year,month) {
   return dayCount;
 }
 
+// ---- FUNZIONE PER PRENDERE I GIORNI ----
 
+function getDay(month,year,day) {
+  var mom=moment();
+  mom.month(month);
+  mom.year(year);
+  mom.date(day);
+
+  var dayCount=mom.daysInMonth();
+
+  var date=mom.format("DD-MMMM-YYYY");
+  return date;
+}
+
+// ---- FUNZIONE PER STAMPARE IL CALENDARIO ----
+
+function calendar(month,year) {
+
+  var dayCount=daysCount(month,year);
+  var calendarList=$("#daylist");
+
+  var template=$("#li-template").html();
+  var compiled= Handlebars.compile(template);
+
+  var mom=moment();
+  mom.month(month);
+  mom.year(year);
+
+  for (var day = 1; day <= dayCount; day++) {
+
+    var tempDate={
+
+      date: getDay(month,year,day)
+    };
+
+    var finalHTML=compiled(tempDate);
+    calendarList.append(finalHTML);
+  }
+}
 
 function init() {
 
@@ -43,6 +81,8 @@ function init() {
   var month=0;
 
   title(month,year);
+
+  calendar(month,year);
 }
 
 $(document).ready(init);
