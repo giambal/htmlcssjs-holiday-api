7c40727d7c841d74259f05f31848761b6039fad3
giambal <gimmy_1996@hotmail.it>
2019-03-20 16:09:56 +0100

    titolo ok

--- script.js
+++ script.js
@@ -18,9 +18,21 @@ function title(month,year) {
   var title=$("#title");
 
   var monthName=getMonthName(month);
+  var dayCount=daysCount(year,month);
+  title.text(monthName + ": " + "1-" + dayCount);
 
-  title.text(monthName);
+}
 
+// ---- FUNZIONE PER CONTARE QUANTI GIORNI HA IL MESE ----
+
+function daysCount(year,month) {
+
+  var mom=moment();
+  mom.month(month);
+  mom.year(year);
+
+  var dayCount=mom.daysInMonth();
+  return dayCount;
 }
 
 
